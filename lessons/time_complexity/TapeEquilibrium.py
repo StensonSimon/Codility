2108fe1c38b757759cbfd50d2831ae50c8fe5f3b
stensonsimon <stensonsimon@gmail.com>
2021-05-30 14:17:49 +0100

    add TapeEquilibrium.py

--- lessons/time_complexity/TapeEquilibrium.py
+++ lessons/time_complexity/TapeEquilibrium.py
@@ -4,17 +4,14 @@ import random
 def solution(A):
     # write your code in Python 3.6
     c =9999999
-    sum1 = sum(A)-A[-1]
-    sum2 = A[-1]
-
-    print(sum1)
-    for i in range(1, len(A)):
-        c = min(abs(sum1-sum2), c)
-        
+    sum1 = sum(A)
+    sum2 = 0
+    while len(A)>1:
+        last_el = A.pop()
+        sum2 = sum2 + last_el
+        sum1 = sum1-last_el
+        diff = abs(sum1-sum2)
+        c = min(c,diff)
     return c
     pass
 
-A=[]
-for i in range(1,10000):
-    A.append(random.randint(0,100))
-print(solution([3,1,2,4,3]))
